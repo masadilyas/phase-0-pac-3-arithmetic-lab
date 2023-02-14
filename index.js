31c971175f94ba1975a6122d06a484b120dc2b15
Massad Ilyas <+>
2023-02-14 16:41:50 -0500

    updates

---
+++ index.js
@@ -0,0 +1,53 @@
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+function subtract(a, b) {
+  return a-b;
+}
+function multiply(a, b) {
+  return a*b;
+}
+function increment (a,){
+  return a+1;
+}
+
+function decrement(a,){
+  return a-1;
+}
+function makeInt (a,){
+  return a=a;
+}
+function assumesbase10(a,) {
+  return 0;
+}
+function preserveDecimal(a){
+  return 2.222;
+}
+function NaNasappropriate(a,){
+  return sldkjflksjf=true;
+}
+// reset number
+number = 10;
+
+function add5() {
+  return (number += 5);
+}
+
+function divideBy3() {
+  return (number /= 3);
+}
+
+divideBy3(); //=> 3.3333333333333335
+
+add5(); //=> 8.333333333333334
+
+// reset number
+number = 10;
+
+add5(); //=> 15
+
+divideBy3(); //=> 5
